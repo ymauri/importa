3a974a99723b460252d81ad794bda92da2a29f0c
ymauri <ymauri@outlook.com>
2020-01-26 22:42:59 +0100

    Customizing home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Client;
+use App\Models\Shipping;
+use App\Order;
+use App\Product;
+
 class HomeController extends Controller
 {
     /**
@@ -21,6 +26,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('dashboard');
+        $orders = Order::all()->count();
+        $shippings = Shipping::all()->count();
+        $clients = Client::all()->count();
+        $products = Product::all()->count();
+        return view('dashboard', compact('orders', 'shippings', 'clients', 'products'));
     }
 }
